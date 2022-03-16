0d718bebb347d4b6ff047ede586b79182c5f2255
Sebastian772 <101675465+Sebastian772@users.noreply.github.com>
2022-03-15 20:22:58 -0400

    Add files via upload

---
+++ Actividad1-AldoManetta.py
@@ -0,0 +1,15 @@
+
+
+Nombres = ["Aldo ","Sebastian "]
+
+Apellidos = ["Manetta ","Balza"]
+
+Agregado = [", Espero tenga un feliz dia"]
+
+
+print("Hola Prog1, Soy " + 
+Nombres[0] + 
+Nombres[1] + 
+Apellidos[0] + 
+Apellidos[1] + 
+Agregado[0])
